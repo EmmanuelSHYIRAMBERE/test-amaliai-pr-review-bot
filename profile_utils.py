b7cd71bc175c7af96840663379537afc5b947506
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2026-08-05 14:56:26 +0200

    Revert get_display_name to direct indexing

--- profile_utils.py
+++ profile_utils.py
@@ -3,6 +3,4 @@
 
 def get_display_name(user_data):
     """Return a display name built from the user's profile data."""
-    first_name = user_data.get('first_name', '')
-    last_name = user_data.get('last_name', '')
-    return f"{first_name} {last_name}".strip()
+    return f"{user_data['first_name']} {user_data['last_name']}"
